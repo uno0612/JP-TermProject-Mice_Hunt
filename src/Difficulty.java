e2deb280c20602d7a0bc23673a6779e7d5bf2dbb
uno0612 <unmoses0612@gmail.com>
2026-04-19 20:56:27 +0900

    edit comment to add amount, speed

--- src/Difficulty.java
+++ src/Difficulty.java
@@ -1,8 +1,12 @@
 //Abstract class for three levels of difficulty
 public abstract class Difficulty {
     //The count of mice, milk and timer delay for each depends on the difficulty level.
+    
+    //amount that pops up
     protected int miceCount;
     protected int milkCount;
+
+    //speed
     protected int miceInterval;
     protected int milkInterval;
 
